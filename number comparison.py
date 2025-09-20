2e7430e40eb6e1e41755f7632d80be0c00eaf61e
Sarkodie1255 <sarkcess099@gmail.com>
2025-09-20 17:12:48 +0000

    Add files via upload

---
+++ number comparison.py
@@ -0,0 +1,10 @@
+while True:
+   x=int(input("Enter a number"))
+   y=int(input("enter another number"))
+ 
+   if x > y :
+         print( x ,'is greater than', y)
+   elif x < y:
+         print(y ,'is greater than', x) 
+   else:
+        print('They are equal',)
